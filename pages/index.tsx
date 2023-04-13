9ad91e9dc5bfeccb2ad9904fd7b133df0cadcfba
M Urwah Shahid <107793187+urwah1248@users.noreply.github.com>
2023-04-13 17:13:06 +0500

    Initial Commit, Next app, Tailwind

---
+++ pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Web Engineering Project</title>
+        <meta name="description" content="E commerce Store" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <main>
+        <h1 className='text-xl font-bold'>HELLO WORLD</h1>
+      </main>
+    </>
+  )
+}
