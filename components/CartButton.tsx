9e601c54bbe17cfae8e497c609dbbb05365ae959
Muhammad Urwah <urwahpatel@gmail.com>
2023-05-31 14:23:10 +0500

    Fixed Deployment error

--- components/CartButton.tsx
+++ components/CartButton.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { ClockCircleOutlined } from '@ant-design/icons';
 import { Avatar, Badge, Space } from 'antd';
 import 'bootstrap-icons/font/bootstrap-icons.css'
+
 interface Props {
   count?: String,
 }
 
-const CartButton: React.FC = ({count,...props}:Props) => (
+const CartButton = (props:Props) => (
   <Space size="middle">
-    <Badge count={count}>
+    <Badge count={props.count}>
       <i className="bi bi-bag text-lg"></i>
     </Badge>
   </Space>
