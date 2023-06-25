a440c2c2daf228b1747ff5ba43b70418533801e6
Muhammad Urwah <urwahpatel@gmail.com>
2023-06-25 18:57:52 +0500

    Updated Update Form

--- components/InputModal.tsx
+++ components/InputModal.tsx
@@ -15,6 +15,7 @@ import {
 import axios from "axios"
 import { useRef, useState } from "react"
 import { Select } from "@chakra-ui/react"
+import { AiFillDelete } from 'react-icons/ai'
 
 interface InputModalProps {
   text: any
@@ -30,6 +31,8 @@ export const InputModal: React.FC<InputModalProps> = ({ text, id }) => {
   const [description, setDescription] = useState<string>()
   const [category, setCategory] = useState<string>()
   const [price, setPrice] = useState<string>()
+  const [stock, setstock] = useState([{ size: '', quantity: 0 }]);
+
   const editProduct = async () => {
     await axios
       .put(`${process.env.NEXT_PUBLIC_API_BASE_URL}product/${id}`, {
@@ -44,6 +47,20 @@ export const InputModal: React.FC<InputModalProps> = ({ text, id }) => {
         console.log(err)
       })
   }
+
+  const handleInputChange = (sizeIndex:any, field:any, event:any) => {
+    const { value } = event.target;
+    const updatedstock:any = [...stock];
+    updatedstock[sizeIndex][field] = value;
+    setstock(updatedstock);
+  };
+  
+  const removeSize = (sizeIndex:number) => {
+    const updatedstock = [...stock];
+    updatedstock.splice(sizeIndex, 1);
+    setstock(updatedstock);
+  };
+
   return (
     <>
       <Button onClick={onOpen}>{text}</Button>
@@ -94,6 +111,41 @@ export const InputModal: React.FC<InputModalProps> = ({ text, id }) => {
                 placeholder="Price"
                 onChange={(e) => setPrice(e.target.value)}
               />
+            </FormControl>
+
+            <FormControl mt={4}>
+              <FormLabel>Stock</FormLabel>
+              {stock.map((size, sizeIndex) => (
+                <div key={sizeIndex} className="border-2 p-3 flex gap-2 items-center">
+                  <FormLabel className="">
+                    Size:
+                    <Input
+                      type="text"
+                      value={size.size}
+                      onChange={(event) =>
+                        handleInputChange(sizeIndex, 'size', event)
+                      }
+                      className="w-full px-4 py-2 mb-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                      
+                    />
+                  </FormLabel>
+                  <FormLabel className="">
+                    Quantity:
+                    <Input
+                      type="number"
+                      value={size.quantity}
+                      onChange={(event) =>
+                        handleInputChange(sizeIndex, 'quantity', event)
+                      }
+                      className="w-full px-4 py-2 mb-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                      />
+                  </FormLabel>
+                  
+                  <button type="button" className="mt-3 h-10 w-10 text-center flex justify-center items-center bg-red-600 rounded-md text-gray-200 hover:bg-red-500 focus:ring-red-600 disabled:bg-red-300" disabled={stock.length<2} onClick={() => removeSize(sizeIndex)}>
+                    <AiFillDelete/>
+                  </button>
+                </div>
+              ))}
             </FormControl>
           </ModalBody>
 
