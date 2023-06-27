5c36d52ddec70f0f5fe20fa5b2362dc7311d482e
Muhammad Urwah <urwahpatel@gmail.com>
2023-06-27 23:53:40 +0500

    Fixed Product update

--- components/InputModal.tsx
+++ components/InputModal.tsx
@@ -16,11 +16,24 @@ import axios from "axios"
 import { useRef, useState } from "react"
 import { Select } from "@chakra-ui/react"
 import { AiFillDelete } from 'react-icons/ai'
+import { getDownloadURL, ref, uploadBytes } from "firebase/storage"
+import { storage } from "@/firebase/Firebase"
 
 interface InputModalProps {
   text: any
   id: any
 }
+
+interface ImagesInterface {
+  original?: File | null
+  thumbnail?: File | null
+}
+
+interface ImageUrlInterface {
+  original?: string
+  thumbnail?: string
+}
+
 export const InputModal: React.FC<InputModalProps> = ({ text, id }) => {
   const { isOpen, onOpen, onClose } = useDisclosure()
 
@@ -32,15 +45,64 @@ export const InputModal: React.FC<InputModalProps> = ({ text, id }) => {
   const [category, setCategory] = useState<string>()
   const [price, setPrice] = useState<string>()
   const [stock, setstock] = useState([{ size: '', quantity: 0 }]);
+  const [images, setImages] = useState<ImagesInterface[]>([])
+  const [imageUrls, setImageUrls] = useState<ImageUrlInterface[]>([])
+
+  const uploadImageToFirebase = async (images: ImagesInterface[]) => {
+    await Promise.all(
+      images.map(async (img: any) => {
+        if (img.original) {
+          const imageRef = ref(
+            storage,
+            `images/product images/${img.original.name}`
+          )
+          return uploadBytes(imageRef, img && img.original).then(
+            async (snapshot) => {
+              return getDownloadURL(snapshot.ref).then((url) => {
+                imageUrls.push({
+                  original: url,
+                  thumbnail: url,
+                })
+                setImageUrls([...imageUrls])
+              })
+            }
+          )
+        }
+      })
+    )
+    return imageUrls
+  }
+  const handleImages = (e: any) => {
+    for (let i = 0; i < e.target.files.length; i++) {
+      var imageFile = e.target.files[i]
+      images.push({
+        original: imageFile,
+        thumbnail: imageFile,
+      })
+      setImages([...images])
+    }
+  }
 
   const editProduct = async () => {
+    await uploadImageToFirebase(images)
+    .catch((err) => {
+      onClose()
+      console.log(err)
+    })
+
+    const requestData: any = {
+      title: title ?? undefined,
+      description: description ?? undefined,
+      category: category ?? undefined,
+      price: price ?? undefined,
+      stock: stock.some((size) => size.quantity > 0)
+        ? stock.map(({ size, quantity }) => ({ size, quantity }))
+        : undefined,
+      images: imageUrls.length > 0 ? imageUrls : undefined,
+    };
+
     await axios
-      .put(`${process.env.NEXT_PUBLIC_API_BASE_URL}product/${id}`, {
-        title,
-        description,
-        category,
-        price,
-      })
+      .put(`${process.env.NEXT_PUBLIC_API_BASE_URL}product/${id}`, requestData)
       .then((_) => onClose())
       .catch((err) => {
         onClose()
@@ -133,19 +195,37 @@ export const InputModal: React.FC<InputModalProps> = ({ text, id }) => {
                     Quantity:
                     <Input
                       type="number"
-                      value={size.quantity}
+                      value={size.quantity>0?size.quantity:''}
                       onChange={(event) =>
                         handleInputChange(sizeIndex, 'quantity', event)
                       }
                       className="w-full px-4 py-2 mb-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
                       />
                   </FormLabel>
-                  
-                  <button type="button" className="mt-3 h-10 w-10 text-center flex justify-center items-center bg-red-600 rounded-md text-gray-200 hover:bg-red-500 focus:ring-red-600 disabled:bg-red-300" disabled={stock.length<2} onClick={() => removeSize(sizeIndex)}>
-                    <AiFillDelete/>
-                  </button>
+                  <FormLabel className="">
+                    <span className="text-transparent">Del</span>
+                    <button type="button" className="border-2 mb-2 py-2 w-10 text-center flex justify-center items-center bg-red-600 rounded-md text-gray-200 hover:bg-red-500 focus:ring-red-600 disabled:bg-red-300" disabled={stock.length<2} onClick={() => removeSize(sizeIndex)}>
+                      <AiFillDelete className="text-xl"/>
+                    </button>
+                  </FormLabel>
                 </div>
               ))}
+
+              <FormLabel
+                htmlFor="images"
+                className="block mb-2 text-lg font-medium text-gray-700"
+              >
+                Upload Images
+              <input
+                type="file"
+                accept="image/*"
+                multiple
+                id="image"
+                onChange={(e) => handleImages(e)}
+                className="w-full px-4 py-2 mb-2 border border-gray-300 rounded focus:outline-none focus:ring-2 focus:ring-indigo-500"
+                required
+              />
+              </FormLabel>
             </FormControl>
           </ModalBody>
 
